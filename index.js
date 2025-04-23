d59c0db942085e39864bc881a5cad8d108eec80c
vivek <vivekgirhepunje3456@gmail.com>
2025-04-23 15:51:46 +0530

    Small Fix

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const path= require('path')
-
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,'/views'));
 
-app.use(express.static(path.join(__dirname,'/public')))
+app.set(express.static(path.join(__dirname,'/public')))
 
 const port = 8080
 app.listen(port,()=>{
     console.log("Server is listening at: ",port)
 })
+
 
